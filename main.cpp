277b3e7f1a28c79fe007fb69598276485851f456
Ali <Ali@ALI-PC>
2019-01-18 14:18:18 +0330

    deleting the first line
    this is my second edit from my desktop.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 
 int main()
 {
-  std::cout << "hello world" << std::endl;
   std::cout << "this is a c++ program" << std::endl;
   std::cout << "this is a testing project" << std::endl;
   return 0;
